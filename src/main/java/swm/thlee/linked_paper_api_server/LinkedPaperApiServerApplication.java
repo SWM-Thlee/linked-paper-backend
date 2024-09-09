e29b2e3a42d707ee82f3b608ff1154771fb3a4f3
ljy2855 <ljy2855@naver.com>
2024-09-09 16:39:51 +0900

    init

---
+++ src/main/java/swm/thlee/linked_paper_api_server/LinkedPaperApiServerApplication.java
@@ -0,0 +1,12 @@
+package swm.thlee.linked_paper_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedPaperApiServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LinkedPaperApiServerApplication.class, args);
+  }
+}
---
+++ src/test/java/swm/thlee/linked_paper_api_server/LinkedPaperApiServerApplicationTests.java
@@ -0,0 +1,11 @@
+package swm.thlee.linked_paper_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedPaperApiServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
