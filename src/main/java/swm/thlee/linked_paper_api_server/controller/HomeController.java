76593c761d1a42add3b33c7ae875fdddf5bd74ca
ljy2855 <ljy2855@naver.com>
2024-10-11 16:11:00 +0900

    hotfix: remove sentry error endpoint

--- src/main/java/swm/thlee/linked_paper_api_server/controller/HomeController.java
+++ src/main/java/swm/thlee/linked_paper_api_server/controller/HomeController.java
@@ -1,6 +1,5 @@
 package swm.thlee.linked_paper_api_server.controller;
 
-import io.sentry.Sentry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +9,5 @@ public class HomeController {
   @GetMapping("/")
   public String home() {
     return "API is running";
-  }
-
-  @GetMapping("/error")
-  public String error() {
-    try {
-      throw new Exception("This is a test.");
-    } catch (Exception e) {
-      Sentry.captureException(e);
-    }
-    return "error";
   }
 }
